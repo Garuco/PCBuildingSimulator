4a56971fdbaa5716422ea7a0859063a67a07db89
Josué Quesada Jiménez <rogelio.rgonzalez@gmail.com>
2024-05-27 22:30:50 -0600

    ramCompatibilidad

---
+++ Assets/Compatibilidad.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compatibilidad : MonoBehaviour
+{
+    public string tipoRAM;
+    public int frecuencia;
+    public int capacidad;  // en GB
+
+    // Método para obtener las propiedades de la RAM
+    public string ObtenerTipo()
+    {
+        return tipoRAM;
+    }
+
+    public int ObtenerFrecuencia()
+    {
+        return frecuencia;
+    }
+
+    public int ObtenerCapacidad()
+    {
+        return capacidad;
+    }
+}
--- Assets/SocketHandler.cs
+++ Assets/SocketHandler.cs
@@ -1,52 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit.Interactors;   
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class SocketHandler : MonoBehaviour
 {
-    public Transform component;
-    public Transform socket;
+    public Transform component; // Este será el componente RAM
+    public Transform socket;    // Este es el socket en la motherboard
+    public RamComp motherboardScript;
 
     private XRSocketInteractor holsterSocket;
     public float activationDistance = 1.5f; // Distancia para activar el Socket
 
-    // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("SocketHandler Start");
         holsterSocket = GetComponent<XRSocketInteractor>();
         holsterSocket.enabled = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Obtener la dirección de las caras
-        Vector3 direccionCaraDelantera = transform.TransformDirection(-component.forward);
-        Vector3 direccionCaraTrasera = transform.TransformDirection(socket.forward);
-
-        // Calcular el producto punto
-        float productoPunto = Vector3.Dot(direccionCaraDelantera.normalized, direccionCaraTrasera.normalized);
-
         float distance = Vector3.Distance(component.position, socket.position);
-        if (distance <= activationDistance)
+        if (distance <= activationDistance && AreComponentsFacingEachOther() && IsRamCompatible())
         {
-            // Si el producto punto es negativo, las caras están una frente a la otra
-            if (productoPunto < -0.7)
-            {
-                //Debug.Log("La cara delantera del Cubo1 está frente a la cara trasera del Cubo2.");
-                holsterSocket.enabled = true;
-            }
-            else
-            {
-                //Debug.Log("Las caras no están una frente a la otra.");
-                holsterSocket.enabled = false;
-            }
+            Debug.Log("Activando Socket");
+            holsterSocket.enabled = true;
         }
         else
         {
+            Debug.Log(distance + " " + AreComponentsFacingEachOther() + " " + IsRamCompatible());
+            Debug.Log("Desactivando Socket");
             holsterSocket.enabled = false;
         }
+    }
 
+    private bool AreComponentsFacingEachOther()
+    {
+
+        Vector3 frontDirection = transform.TransformDirection(-component.forward);
+        Vector3 backDirection = transform.TransformDirection(socket.forward);
+        float dotProduct = Vector3.Dot(frontDirection.normalized, backDirection.normalized);
+
+        return dotProduct < -0.7; // Cara delantera del componente está frente a la cara trasera del socket
+    }
+
+    private bool IsRamCompatible()
+    {
+        Debug.Log("Verificando compatibilidad");
+        Compatibilidad ramScript = component.GetComponent<Compatibilidad>();
+        Debug.Log("Verificando compatibilidad de la RAM" + ramScript.capacidad);
+        Debug.Log("Verificando compatibilidad de la RAM" + motherboardScript.capacidadMaxima);
+        Debug.Log("Verificando compatibilidad de la RAM" + motherboardScript.frecuenciaMaxima);
+        Debug.Log("Verificando compatibilidad de la RAM" + motherboardScript.tipoCompatible);
+        if (ramScript != null && motherboardScript != null)
+        {
+            Debug.Log("Verificando compatibilidad de la RAM");
+            return motherboardScript.VerificarCompatibilidad(component.gameObject);
+        }
+        return false;
     }
 }
