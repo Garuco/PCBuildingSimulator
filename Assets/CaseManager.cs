f3222f84ad4ab2f2b7d188bd8157e2648d2ae8db
CharlieCharlieX <edsoli94@gmail.com>
2024-06-04 01:29:38 -0600

    Compatible Components Done

--- Assets/CaseManager.cs
+++ Assets/CaseManager.cs
@@ -6,16 +6,19 @@ using UnityEngine;
 public class CaseManager : MonoBehaviour
 {
     public GetMotherboard getMotherboard;
-    public MotherboardManager mbManager;
+    private MotherboardManager mbManager;
+
+
     public List<string> getMissingComponents()
     {
         List<string> list = new List<string>();
-        if(getMotherboard.getMotherboard() == null)
+        if (getMotherboard.getMotherboard() == null)
         {
             list.Add("No se ha localizado el componente: Tarjeta madre");
         }
         else
         {
+            mbManager = getMotherboard.getMotherboard().GetComponent<MotherboardManager>();
             List<string> listComponents = mbManager.GetComponents();
             if (listComponents.Count > 0)
             {
